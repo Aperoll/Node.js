dce38ae186ebd35742761d013461224e4fd45b33
Aperoll <april.jung@gmail.com>
2015-04-06 03:44:22 -0500

    Delete API_Practice.js

--- API_Practice.js
+++
@@ -1,4 +0,0 @@
-var https = require("https");
-https.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/AppleCrisps?api_key=20190fb1-47f3-4d18-a8c4-cf768cabba44", function(response) {
-  console.log(response.statusCode);
-});
