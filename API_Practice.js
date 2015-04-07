36f7c3986670405edb0f194a24296f09ee4c9d0e
Aperoll <april.jung@gmail.com>
2015-04-07 02:39:57 -0500

    Update API_Practice.js

--- API_Practice.js
+++ API_Practice.js
@@ -1,5 +1,26 @@
+function printMessage(name, summonerLevel, id) {
+  var messageName = "Summoner Name: " + name;
+  var messageLevel = "Summoner Level: " + summonerLevel;
+  var messageId = "Summoner ID: " + id;
+  console.log(messageName);
+  console.log(messageLevel);
+  console.log(messageId);
+}
+
 var https = require("https");
-https.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/AppleCrisps?[API_KEY]", function(response) {
-  console.log(response.statusCode);
+var request = https.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/AppleCrisps?[API_KEY]", function(response) {
+var body = "";
+  
+  response.on("data", function(chunk) {
+    body += chunk;
+    });
+  response.on("end", function(){
+    var profile = JSON.parse(body);
+    printMessage(profile.applecrisps.name, profile.applecrisps.summonerLevel, profile.applecrisps.id)
+  });
+});
+
+request.on("error", function(error) {
+  console.error(error.message);
 });
 
